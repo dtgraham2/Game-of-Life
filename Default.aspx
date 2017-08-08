08aa76dba9c847148fc528a072be5bfba8bf43ef
dtgraham2 <dtg2@students.ptcollege.edu>
2017-08-08 12:12:13 -0400

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="font-size: 24">
+        <asp:Button ID="myButton" Text="Next Gen" OnClick="runGen" runat="server" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    char[,] Population = {     { 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+                               { 'x', 'x', 'x', 'x', 'x', '.', 'x', 'x', 'x', 'x'},
+                               { 'x', 'x', 'x', '.', '.', '.', 'x', 'x', 'x', 'x'},
+                               { 'x', '.', 'x', 'x', 'x', 'x', 'x', 'x', '.', 'x'},
+                               { 'x', '.', '.', '.', '.', '.', '.', '.', '.', 'x'},
+                               { 'x', 'x', 'x', '.', 'x', 'x', '.', 'x', 'x', 'x'},
+                               { 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+                               { 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+                               { 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+                               { 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'}};
+    int columns = 9;
+    int rows = 9;
+    bool first;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Page.IsPostBack)
+        {
+            Population = (char[,])Session["population"];
+        }
+        else
+        {
+            Session.Add("population", Population);
+        }
+    }
+
+
+    public void runGen(object sender, EventArgs e)
+    {
+        char[,] tempStorage = new char[rows + 1, columns + 1];
+
+        Response.Write("<code>");
+            for (int i = 0; i <= columns; i++)
+            {
+                for (int j = 0; j <= rows; j++)
+                {
+                    
+                    int neighborsAlive = 0;
+                    if (i > 0 && j > 0)
+                    {
+                        if (Population[i - 1, j - 1] == '.')
+                            neighborsAlive++;
+                    }
+                    if (j > 0)
+                    {
+                        if (Population[i, j - 1] == '.')
+                            neighborsAlive++;
+                    }
+                    if (i > 0)
+                    {
+                        if (Population[i - 1, j] == '.')
+                            neighborsAlive++;
+                    }
+                    if (i > 0 && j < columns)
+                    {
+                        if (Population[i - 1, j + 1] == '.')
+                            neighborsAlive++;
+                    }
+                    if (i < columns && j > 0)
+                    {
+                        if (Population[i + 1, j - 1] == '.')
+                            neighborsAlive++;
+                    }
+                    if (j < columns)
+                    {
+                        if (Population[i, j + 1] == '.')
+                            neighborsAlive++;
+                    }
+                    if (i < columns)
+                    {
+                        if (Population[i + 1, j] == '.')
+                            neighborsAlive++;
+                    }
+
+                    if (i < columns && j < columns)
+                    {
+                        if (Population[i + 1, j + 1] == '.')
+                            neighborsAlive++;
+                    }
+
+                    if (neighborsAlive == 2 && Population[i, j] == '.')
+                    {
+                        tempStorage[i, j] = '.';
+                    }
+                else
+                {
+                    tempStorage[i, j] = 'x';
+                }
+
+                    if (neighborsAlive == 3)
+                    {
+                        tempStorage[i, j] = '.';
+                    }
+                    if (neighborsAlive < 2 || neighborsAlive > 3)
+                    {
+                        tempStorage[i, j] = 'x';
+                    }
+            }
+        }
+
+        for (int i = 0; i <= rows; i++)
+        {
+            for(int j = 0; j <= columns; j++)
+            {
+                Population[i, j] = tempStorage[i, j];
+                Response.Write(Population[i, j] + " ");
+            }
+            Response.Write("<br/>");
+        }
+
+    }   
+}
